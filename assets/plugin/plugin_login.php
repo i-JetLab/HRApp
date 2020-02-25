19c6a1c62daf847f3c51dca0ccf60c2f7a45f52a
Maxwell Newberry <mnewber1@kent.edu>
2020-02-25 14:14:39 -0600

    Update plugin_login.php

--- assets/plugin/plugin_login.php
+++ assets/plugin/plugin_login.php
@@ -6,6 +6,10 @@
    * Reference: Maxwell Newberry (i-Jet)
    */
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
     session_start();
 
     if(isset($_POST['badge'])) {

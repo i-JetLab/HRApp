d38666e0ec146a378a0a7f5f2da39617ea3c5c07
Maxwell Newberry <mnewber1@kent.edu>
2020-02-25 14:23:02 -0600

    Update verify.php

--- assets/plugin/verify.php
+++ assets/plugin/verify.php
@@ -4,9 +4,9 @@ require '../f/db.func.php';
 session_start();
 
 if(isset($_GET['sid'])) {
-  $s = DB::prepare("UPDATE `login_validate` SET `validated`=1 WHERE `session`=:session");
+  $s = DB::prepare("UPDATE login_validate SET validated=1 WHERE session=:session");
   $s->execute(['session' => $_GET['sid']]);
-  $uid = DB::query("SELECT `uid` FROM `login_validate` WHERE `session`='" . $_GET['sid'] . "' LIMIT 1")->fetch()['uid'];
+  $uid = DB::query("SELECT uid FROM login_validate WHERE session='" . $_GET['sid'] . "' LIMIT 1")->fetch()['uid'];
   $_SESSION['login'] = $uid;
   echo "Done, please close window.";
 }

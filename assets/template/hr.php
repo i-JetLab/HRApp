745397238ea29dfff40e33235fdd678f031001f8
Maxwell Newberry <mnewber1@kent.edu>
2020-03-10 13:28:44 -0500

    Update hr.php

--- assets/template/hr.php
+++ assets/template/hr.php
@@ -32,7 +32,7 @@
                   }
               }
           
-              if(isset($_GET['bid_id']) && isset($_GET['action') && $_GET['action'] == "confirm_winner") {
+              if(isset($_GET['bid_id']) && isset($_GET['action']) && $_GET['action'] == "confirm_winner") {
                 // If both variables are set and are != ""
                 $employee_info = DB::query("SELECT * FROM winners WHERE bid = {$_GET['bid_id']}")->fetch();
                 var_dump($employee_info);
